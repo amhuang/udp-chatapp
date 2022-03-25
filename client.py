0ba20726b8eaa68faff0e41d4ad8aad4c7318e2e
Andrea <amhuang00@gmail.com>
2022-03-25 11:20:05 +0000

    direct chat implemented

--- client.py
+++ client.py
@@ -9,72 +9,102 @@ import sys
 
 table = [['name','ip','port','status']]
 lock = threading.Lock()
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-online = False
-exit = False
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+online = True
 
 def run(name, server_ip, server_port, client_port):
     global online
-    sock.bind((socket.gethostname(), client_port))
-    sock.connect((server_ip, server_port))
+    udp.bind((socket.gethostname(), client_port))
+    s.bind((socket.gethostname(), client_port))
+    s.connect((server_ip, server_port))
+    
+    # starts thread for receiving msgs from server
+    server_comm = threading.Thread(target=recv_server)
+    server_comm.start()
+
+    # starts thread for direct chat
+    dir_chat = threading.Thread(target=recv_chat, args=())
+    dir_chat.start()
+
     register(name)
 
-    receiving_th = threading.Thread(target=receiving, args=())
-    receiving_th.start()
-
-    # Loop for processing incoming messages
+    # Processes user input
     while True:
-        print(">>> ", end="", flush=True)
-        data = input()
-        if data == "dereg " + name and online:
+        print(">>>", end=" ", flush=True)
+        cmd = input()
+
+        if len(cmd) > 5 and cmd[:5] == "send ":
+            name, msg = clean_send(cmd[5:])
+            send_chat(name, msg)
+        
+        elif len(cmd) > 9 and cmd[0:9] == "send_all ":
+            name = clean_send(cmd[9:], group=True)
+
+        elif cmd == "dereg " + name and online:
             print("deregistering", name)
             dereg_timeout(name)
             
-        elif data == "reg " + name:
+        elif cmd == "reg " + name:
             if not online:
-                print("registering", name)
                 register(name)
             else:
                 print(">>> [Client already registered.]")
 
-        else:
-            print('''help''')
+        elif cmd == "help":
+            help_msg = 'CHATAPP COMMANDS\n\tsend <name> <message>   Direct chat: end a message to one user\n\tsend_all <message>      Group chat: send message to all clients\n\tdereg <nickname>        Deregister (go offline)\n\treg <nickname>          Re-register (return online)'
+            print(help_msg)
 
-# Run by receiving_thread
-def receiving():
-    global online, exit
+
+# Run by server_comm thread. Processes messages from server
+def recv_server():
+    global online
     while True:
-
         if online:
-            msg = sock.recv(4).decode()
-            if exit: 
-                sys.exit(1)
-                
+            msg = s.recv(4).decode()
             print("from recving:", msg)
             lock.acquire()
             
             if msg == '':   # Server disconnected
-                sock.close()
+                s.close()
                 break 
 
             if msg == "tab\n":
                 table_updated()
+            
             elif msg == "der\n":
-                ack = sock.recv(2).decode()
+                ack = s.recv(2).decode()
                 print("recieved dereg ack", ack)
                 if ack == "OK":
                     online = False
             else: 
-                msg = sock.recv(1024)
+                msg = s.recv(1024)
             lock.release()
-            print("recv lock released")
 
-def table_updated():
-    msg = sock.recv(1024)
-    table = pickle.loads(msg)
-    print(">>> [Client table updated.]")
-    print(tabulate(table))
-            
+def recv_chat():
+    while True:
+        buf, addr = udp.recvfrom(1024)
+        for row in table:
+            if row[1] == addr[0] and row[2] == addr[1]:
+                print(row[0], end=": ")
+        print(buf.decode())
+        print(">>>", end=" ", flush=True)
+
+def send_chat(name, msg):
+    recipient = []
+    for row in table:
+        if row[0] == name:
+            recipient = row
+
+    if recipient == []:
+        print(">>> [Recipient not found.]")
+        return
+    elif recipient[3] == "no":
+        pass
+    
+    msg = msg.encode()
+    udp.sendto(msg, (recipient[1], recipient[2]))
+    print("message sent to", str((recipient[1], recipient[2])))
 
 def register(name):
     global online
@@ -83,20 +113,21 @@ def register(name):
     lock.acquire()
     print("Registering", name)
     msg = "reg\n" + name
-    sock.sendall(msg.encode())
+    s.sendall(msg.encode())
     print(">>> [Welcome, You are registered.]", flush=True)
     '''else: 
         print(">>> [Register failed]", flush=True)
-        sock.close()'''
+        s.close()'''
     # Releases lock acquired when deregistering
     lock.release()
 
+
 def dereg_timeout(name):
-    global online, exit
+    global online
     msg = ("der\n" + name).encode()
 
     def send_dereg():
-        sock.sendall(msg)
+        s.sendall(msg)
         
     # Attempt to send dereg 5 times
     timeout = 1
@@ -118,6 +149,31 @@ def dereg_timeout(name):
     else: 
         print(">>> [Server not responding]", flush=True)
         print(">>> [Exiting]", flush=True)
-        sock.close()
+        s.close()
         exit = True
         sys.exit(1)
+
+
+def table_updated():
+    global table
+    msg = s.recv(1024)
+    table = pickle.loads(msg)
+    print(">>> [Client table updated.]")
+    print(tabulate(table))
+    print(">>>", end=" ", flush=True)
+
+
+def clean_send(data, group=False):
+    # strips whitespace off name for group chats
+    if group:
+        return data.strip()
+
+    # Returns name and msg for direct chats
+    name, msg = data.split(" ", 1)
+    name = name.strip()
+    msg = msg.strip()
+    if len(name) == 0:
+        print(">>> [Please specify a user to chat with.]")
+    if len(msg) == 0:
+        print(">>> [Please enter a message to send.]")
+    return name, msg
